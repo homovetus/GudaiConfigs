8f0b5dc0f0cef26a3dad0c8597f336eaa9e38308
Shi Rao <github@homovetus.com>
2024-07-03 13:10:28 +0800

    new plugin

--- .config/nvim/lua/plugins/nvim.lua
+++ .config/nvim/lua/plugins/nvim.lua
@@ -81,6 +81,10 @@ return {
 			label = { uppercase = false },
 		},
 	},
+        {
+                "lewis6991/gitsigns.nvim",
+                opts = {},
+        },
 	{
 		"echasnovski/mini.pairs",
 		opts = {},
