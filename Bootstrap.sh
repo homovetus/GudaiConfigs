6724fe1c9440f1c3b2a679cfb71f8a733b29a9f7
Shi Rao <github@homovetus.com>
2025-10-26 17:24:35 +0800

    change Surge profile name

--- Bootstrap.sh
+++ Bootstrap.sh
@@ -70,5 +70,5 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	printf "\nFiles for Mac:\n"
 	copy_ssh_keys	"$HOME/Documents/SSHKeys/homovetus"
 	copy_ssh_keys	"$HOME/Documents/SSHKeys/homovetus.pub"
-	create_symbolic_link	"Surge.conf"	"$HOME/Library/Application Support/Surge/Profiles/Surge.conf"
+	create_symbolic_link	"Surge.conf"	"$HOME/Library/Application Support/Surge/Profiles/Default.conf"
 fi
