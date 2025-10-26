e75e69fbd919fa5fdf84ca86f38abdc78915ce8c
Shi Rao <github@homovetus.com>
2025-10-26 16:17:38 +0800

    new bootstrap script

--- Bootstrap.sh
+++ Bootstrap.sh
@@ -1,58 +1,73 @@
 #!/usr/bin/env bash
 
-debug=false
+dry=false
 
 while [[ $# -gt 0 ]]; do
         case $1 in
-        -d | --debug) debug=true ;;
+	-d | --dry-run)
+		dry=true ;;
         *)
-                echo "Usage: $0 [-d | --debug]"
-                exit 1
-                ;;
+		printf "Usage: %s [-d | --dry-run]\n" "$0" >&2
+		exit 1 ;;
         esac
         shift
 done
 
 function create_symbolic_link {
-        local path="$1"
-        local target="$2"
-        local config_dir="${3:-$(pwd)}"
+	local source_dir="${3:-.}"
+	local source_path=$(realpath "$source_dir/$1")
+	local target_path="$2"
 
-        target=$(realpath "$config_dir/$target")
+	if [ ! -e "$source_path" ]; then
+		printf "Source '%s' does not exist. Skipping.\n" "$source_path" >&2
+		return 1
+	fi
 
-        if [ "$debug" = true ]; then
-                echo "Will be linking: $path -> $target"
-        else
-                ln -sfv "$target" "$path"
-        fi
+	if [ -d "$source_path" ]; then
+		if [ "$dry" = true ]; then
+			printf "Will be linking contents of directory: '%s' => '%s'\n" "$target_path" "$source_path"
+		else
+			printf "Linking contents of directory: '%s' => '%s'\n" "$target_path" "$source_path"
+			mkdir -p "$target_path"
+			find "$source_path" -mindepth 1 -maxdepth 1 | while read -r item; do
+				printf "\tLinking item: '%s/%s' => '%s'\n" "$target_path" "$(basename "$item")" "$item"
+				ln -sf "$item" "$target_path/"
+			done
+		fi
+	else
+		if [ "$dry" = true ]; then
+			printf "Will be linking file: '%s' => '%s'\n" "$target_path" "$source_path"
+		else
+			printf "Linking file: '%s' => '%s'\n" "$target_path" "$source_path"
+			mkdir -p "$(dirname "$target_path")"
+			ln -sf "$source_path" "$target_path"
+		fi
+	fi
 }
 
 function copy_ssh_keys {
-        local target="$1"
-        local path=""
+	local source=$(realpath "$1")
+	local target="$HOME/.ssh/$(basename "$source")"
 
-        path="$HOME/.ssh/$(basename "$target")"
-        target=$(realpath "$target")
-
-        if [ "$debug" = true ]; then
-                echo "Will be copying: $target -> $path"
-        else
-                cp -fv "$target" "$path"
-        fi
+	if [ "$dry" = true ]; then
+		printf "Will be copying SSH key: %s -> %s\n" "$source" "$target"
+	else
+		printf "Copying SSH key: %s -> %s\n" "$source" "$target"
+		cp -f "$source" "$target"
+	fi
 }
 
 # Common Files
-echo "Files for all platforms:"
-create_symbolic_link "$HOME/.config" ".config"
-create_symbolic_link "$HOME/.gitconfig" ".gitconfig"
-create_symbolic_link "$HOME/.ideavimrc" ".ideavimrc"
-create_symbolic_link "$HOME/.ssh/config" "ssh_config"
-create_symbolic_link "$HOME/.vimrc" ".vimrc"
-echo ""
+printf "Files for all platforms:\n"
+create_symbolic_link	".config"	"$HOME/.config"
+create_symbolic_link	".gitconfig"	"$HOME/.gitconfig"
+create_symbolic_link	".ideavimrc"	"$HOME/.ideavimrc"
+create_symbolic_link	".vimrc"	"$HOME/.vimrc"
+create_symbolic_link	".zshrc"	"$HOME/.zshrc"
+create_symbolic_link	"ssh_config"	"$HOME/.ssh/config"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-        echo "Files for Mac:"
-        copy_ssh_keys "$HOME/Documents/SSHKeys/homovetus"
-        copy_ssh_keys "$HOME/Documents/SSHKeys/homovetus.pub"
-        create_symbolic_link "$HOME/.zshrc" ".zshrc"
+	printf "\nFiles for Mac:\n"
+	copy_ssh_keys	"$HOME/Documents/SSHKeys/homovetus"
+	copy_ssh_keys	"$HOME/Documents/SSHKeys/homovetus.pub"
 fi
