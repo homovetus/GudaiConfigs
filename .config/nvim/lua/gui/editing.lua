a1683ea2f79ed6f23f30fefa58c22dddfdd07808
Shi Rao <github@homovetus.com>
2025-11-26 00:47:13 +0800

    new plugin for gui

--- .config/nvim/lua/gui/editing.lua
+++ .config/nvim/lua/gui/editing.lua
@@ -1,7 +1,20 @@
 return {
-        {
-                "kylechui/nvim-surround",
-                event = "VeryLazy",
-                opts = {},
-        },
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter", },
+			{ "r", mode = { "x", "o" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash", },
+		},
+		opts = {
+			labels = "arstdhneioqwfpgjluyzxcvbkm",
+			label = { uppercase = false },
+		},
+	},
+	{
+		"kylechui/nvim-surround",
+		event = "VeryLazy",
+		opts = {},
+	},
 }
